951713c07676066df95cf985dcf87036234f70c6
ilia Sotnikov <ilia12st@yandex.ru>
2023-10-13 15:15:42 +0300

    was able to declare a structure in header file ura

--- lib/queue.h
+++ lib/queue.h
@@ -1,12 +1,24 @@
 #pragma once
 #ifndef HEADER_H
 #define HEADER_H
+#include <stdbool.h>
 
 //queue element
-typedef struct QueueElem;
+struct QueueElem
+{
+    int value;
+    struct QueueElem* next;
+    struct QueueElem* previous;
+};
+typedef struct QueueElem QueueElem;
 
 //queue
-typedef struct Queue;
+struct Queue
+{
+    struct QueueElem* end;
+    struct QueueElem* head;
+};
+typedef struct Queue Queue;
 
 //adds an item to the end of the queue
 int enqueue();
@@ -15,10 +27,10 @@ int enqueue();
 int dequeue();
 
 //checks if the queue is empty
-bool isEmpty(Queue *queue);
+bool isEmpty();
 
 //deletes a queue
-void deleteQueue(Queue* queue);
+void deleteQueue();
 
 //returns the value of the top element
 int front();
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+#include "lib/queue.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    return 0;
+    Queue* queue = malloc(sizeof(Queue));
+    printf("PABEDA");
 }
