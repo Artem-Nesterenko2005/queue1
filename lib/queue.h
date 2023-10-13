93a71ad28489a58af84d140235472dab3ac22478
ilia Sotnikov <ilia12st@yandex.ru>
2023-10-13 14:42:09 +0300

    add function declarations

--- lib/queue.h
+++ lib/queue.h
@@ -1,1 +1,41 @@
 #pragma once
+#ifndef HEADER_H
+#define HEADER_H
+
+//queue element
+typedef struct QueueElem
+{
+    int value;
+    QueueElem *next;
+    QueueElem *previous;
+} QueueElem;
+
+//queue
+typedef struct Queue
+{
+    QueueElem end;
+    QueueElem head;
+    
+} Queue;
+
+//adds an item to the end of the queue
+int enqueue();
+
+//removes the first item from the queue and returns its value
+int dequeue();
+
+//checks if the queue is empty
+bool isEmpty(Queue *queue);
+
+//deletes a queue
+void deleteQueue(Queue* queue);
+
+//returns the value of the top element
+int front();
+
+//returns the value of the last element
+int back();
+
+//outputs a queue
+void printQueue();
+#endif
